d902c78cf8c674153752654dd5e6cdce013a8a49
trusteddev1409 <trusteddev1325@gmail.com>
2022-04-13 15:25:22 +0900

    add build

---
+++ build/static/js/54.9ec2eda2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@minimal/minimal-kit-react"]=this["webpackJsonp@minimal/minimal-kit-react"]||[]).push([[54],{1528:function(i,a,t){"use strict";t.r(a);var m=t(2239);a.default=m.a}}]);
+//# sourceMappingURL=54.9ec2eda2.chunk.js.map
